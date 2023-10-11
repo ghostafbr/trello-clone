77b50fcab40e552cb969562c3d02ed849e7dd18f
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-11 15:38:01 -0500

    Add accordions

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -12,5 +12,47 @@ export class BoardsComponent {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
+
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Subitem 1.1',
+        },
+        {
+          label: 'Subitem 1.2',
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Subitem 2.1',
+        }
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Subitem 3.1',
+        },
+        {
+          label: 'Subitem 3.2',
+        },
+        {
+          label: 'Subitem 3.3',
+        }
+      ]
+    }
+  ];
 
 }
