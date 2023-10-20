70ca824cec1d902d8d2fe6f79a048048a5dd8a68
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-19 20:06:35 -0500

    Add Interceptor

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,6 +6,7 @@ import {BehaviorSubject, switchMap, tap} from "rxjs";
 import {TokenService} from "@services/token.service";
 import {ResponseLogin} from "@models/auth.model";
 import {User} from "@models/user.model";
+import {checkToken} from "@interceptors/token.interceptor";
 
 @Injectable({
   providedIn: 'root'
@@ -30,7 +31,9 @@ export class AuthService {
       email,
       password
     }).pipe(
-      tap(( response ) => { this.tokenService.saveToken(response.access_token);})
+      tap((response) => {
+        this.tokenService.saveToken(response.access_token);
+      })
     );
   }
 
@@ -66,11 +69,7 @@ export class AuthService {
 
   getProfile() {
     const token = this.tokenService.getToken();
-    return this.http.get<User>(`${this.apiUrl}/api/v1/auth/profile`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }).pipe(
+    return this.http.get<User>(`${this.apiUrl}/api/v1/auth/profile`, {context: checkToken()}).pipe(
       tap((user) => this.user$.next(user))
     );
   }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "@environments/environment";
 import {TokenService} from "@services/token.service";
 import {User} from "@models/user.model";
+import {checkToken} from "@interceptors/token.interceptor";
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,10 @@ export class UsersService {
   apiUrl = environment.API_URL;
   private tokenService = inject(TokenService);
 
-  constructor() { }
+  constructor() {
+  }
 
   getUsers() {
-    return this.http.get<User[]>(`${this.apiUrl}/api/v1/users`, {
-      headers: {
-        'Authorization': `Bearer ${this.tokenService.getToken()}`
-      }
-    });
+    return this.http.get<User[]>(`${this.apiUrl}/api/v1/users`, {context: checkToken()});
   }
 }
